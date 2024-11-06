7580b862d33c37d21998aa3c7a22e9e5991fc439
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-11-06 09:29:06 +0100

    Initial Commit

---
+++ B21_04_02/B21_04_02App.swift
@@ -0,0 +1,17 @@
+//
+//  B21_04_02App.swift
+//  B21_04_02
+//
+//  Created by Till Hemmerich on 06.11.24.
+//
+
+import SwiftUI
+
+@main
+struct B21_04_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
